ad244b5da96a178d4c1973e4e2d0464b61ce7d5e
airloaf <vikzilla18@gmail.com>
2017-11-04 15:26:10 -0400

    Added basic Graph creator

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello UB Hacking 2017")
+import PIL.Image as Image
+
+import BasicNodeTrasposer
+
+image = Image.open("small.bmp")
+image = image.convert('RGB')
+
+width, height = image.size;
+
+#for col in range(0, width):
+#    print(str(col) + " " + str(image.getpixel((col, 0))))
+
+BasicNodeTrasposer.get_graph(image)
