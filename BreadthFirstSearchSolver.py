59f010c208f20c917bbde5e6491604560acb9b60
Vikram Singh <vikzilla18@gmail.com>
2017-11-04 21:21:03 -0400

    Refactored Breadth first search

--- BreadthFirstSearchSolver.py
+++ BreadthFirstSearchSolver.py
@@ -1,25 +1,37 @@
+import PIL.Image as Image
 
 def bfs(graph, visited, queue, parent):
+    #Get the current node
     current = queue[0]
     queue.pop(0)
-    visited.append(current)
+    visited[current] = True
 
+    #Get the neighbors
     neighbors = graph[current]
 
+    #For each neighbor of the node
     for neighbor in neighbors:
+        #Check if it has not been visited or queued
         if neighbor not in visited and neighbor not in queue:
             queue.append(neighbor)
             parent[neighbor] = current
 
+    #If there is more queued, then BFS more
     if len(queue) > 0:
         bfs(graph, visited, queue, parent)
     else:
         return
 
+def get_nodes(image):
 
-def get_queued_nodes(graph, start, end):
+    #Create a graph
+    graph = create_graph(image)
 
-    visited = []
+    #Get the end points
+    start, end = get_ends(image)
+
+    #BFS Helpers
+    visited = {}
     queue = []
     parent = {}
 
@@ -36,3 +48,72 @@ def get_queued_nodes(graph, start, end):
         par = parent[par]
 
     return nodeQueue
+
+'''
+Get the entrances and exits of images
+'''
+
+def get_ends(image):
+    width, height = image.size
+
+    topRow = 0
+    botRow = height - 1
+
+    topCoord = None
+    botCoord = None
+
+    for col in range(0, width):
+        tCoord = (col, topRow)
+        bCoord = (col, botRow)
+        if image.getpixel(tCoord) == (255, 255, 255):
+            topCoord = tCoord
+        if image.getpixel(bCoord) == (255, 255, 255):
+            botCoord = bCoord
+
+    return (topCoord, botCoord)
+
+'''
+Transpose the image into a graph
+'''
+
+def create_graph(image):
+    graph = transpose_nodes(image)
+    get_neighbors(graph)
+    return graph
+
+def get_neighbors(graph):
+    for coord in graph:
+        #Check the north south west and east neighbors
+        north = (coord[0]-1, coord[1])
+        south = (coord[0]+1, coord[1])
+        west = (coord[0], coord[1] - 1)
+        east = (coord[0], coord[1] + 1)
+
+        #Check if the neighbors are in the graph
+        #Append them if so
+        if north in graph:
+            graph[coord].append(north)
+        if south in graph:
+            graph[coord].append(south)
+        if west in graph:
+            graph[coord].append(west)
+        if east in graph:
+            graph[coord].append(east)
+
+def transpose_nodes(image):
+    width, height = image.size
+
+    graph = {}
+    for row in range(0, height):
+        for col in range(0, width):
+
+            # Coordinate of the
+            coord = (col, row)
+            color = image.getpixel(coord)
+
+            # Check if the coordinate is white
+            if color == (255, 255, 255):
+                #Add an empty list to the coordinates
+                graph[coord] = []
+
+    return graph
--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import PIL.Image as Image
 import sys;
 
-import BasicNodeTrasposer
 import BreadthFirstSearchSolver
 import PaintImage
 
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(5000)
 
 image = Image.open("medmaze.bmp")
 image = image.convert('RGB')
 
-width, height = image.size;
-
-#Get the graph
-graph = BasicNodeTrasposer.get_graph(image)
-
-#Get endpoints
-start, end = BasicNodeTrasposer.getEnds(image)
-
-#Create a queue
-queue = BreadthFirstSearchSolver.get_queued_nodes(graph, start, end);
-
+#get the queue of nodes
+queue = BreadthFirstSearchSolver.get_nodes(image)
 PaintImage.paint(image, queue, "new.bmp")
